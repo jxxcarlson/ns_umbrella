e8e1be1b025a10b4f4b5fafc43bea953fa579ac7
James Carlson <jxxcarlson@gmail.com>
2017-04-23 06:49:23 -0400

    Remove *.sh files from repo

--- capture_and_restore.sh
+++
@@ -1,5 +0,0 @@
-# Capture and download backup. Then upload it to local database
-rm latest.dump
-heroku pg:backups capture
-heroku pg:backups download
-pg_restore --verbose --clean --no-acl --no-owner -h localhost -U carlson -d lookup_repo latest.dump
--- update.sh
+++
@@ -1,1 +0,0 @@
-git push; git push heroku; run
