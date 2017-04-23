b3b1a581e7652674da4c5600012378d58958f1b9
James Carlson <jxxcarlson@gmail.com>
2017-04-23 07:31:28 -0400

    Make elixir version in lookup_phoenix compatible - use v 1.4.2

--- apps/lookup_phoenix/mix.exs
+++ apps/lookup_phoenix/mix.exs
@@ -4,7 +4,7 @@ defmodule LookupPhoenix.Mixfile do
   def project do
     [app: :lookup_phoenix,
      version: "0.0.1",
-     elixir: "1.4.0",
+     elixir: "1.4.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_path: "../../_build",
