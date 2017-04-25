2da5f0254bdcf942c0e47c72f00b6ecc239cfe8c
James Carlson <jxxcarlson@gmail.com>
2017-04-24 21:29:20 -0400

    Fix use of environments to determine the host

--- apps/mu_server/config/config.exs
+++ apps/mu_server/config/config.exs
@@ -8,7 +8,7 @@ use Mix.Config
 # if you want to provide default values for your application for
 # 3rd-party users, it should be done in your "mix.exs" file.
 
-config :mu_server, host: "http://localhost:4001"
+# config :mu_server, host: "http://localhost:4001"
 
 # You can configure for your application as:
 #
