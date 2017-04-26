a9e8f08fab64a5469b98dae89032f7907571b918
James Carlson <jxxcarlson@gmail.com>
2017-04-26 17:03:49 -0400

    define Channel.normalize (new module)

---
+++ apps/lookup_phoenix/lib/channel.ex
@@ -0,0 +1,25 @@
+defmodule LookupPhoenix.Channel do
+
+
+  @doc """
+  iex> Channel.nomalize("demo.all")
+  ["demo", "all"]
+
+  iex> Channel.nomalize("demo.foo.all")
+  ["demo", "foo", "all"]
+
+  iex> Channel.nomalize("demo.foo.all")
+  "demo", "public"]
+"""
+  def normalize(channel) do
+    parts = String.split(channel, ".")
+    site_name = Enum.at(parts, 0)
+    tag = Enum.at(parts, -1)
+    if tag == site_name do
+      tag = "public"
+    end
+    [site_name, tag]
+  end
+
+
+end
