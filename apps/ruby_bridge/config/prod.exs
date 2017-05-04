237b1b62121a9ce794c445a343ebb0420e0586a1
James Carlson <jxxcarlson@gmail.com>
2017-05-03 20:50:39 -0400

    Reconfigure path to asciidoctor-latex

--- apps/ruby_bridge/config/prod.exs
+++ apps/ruby_bridge/config/prod.exs
@@ -4,7 +4,8 @@ use Mix.Config
 
 
 config :ruby_bridge, asciidoc_path: "/app/apps/ruby_bridge/priv/ruby/asciidoc"
-config :ruby_bridge, asciidoctor_latex: "/app/vendor/bundle/ruby/2.3.0/bundler/gems/asciidoctor-latex-b5c9de1363de/bin/asciidoctor-latex"
+# config :ruby_bridge, asciidoctor_latex: "/app/vendor/bundle/ruby/2.3.0/bundler/gems/asciidoctor-latex-b5c9de1363de/bin/asciidoctor-latex"
+config :ruby_bridge, asciidoctor_latex: "/app/vendor/bundle/bin/asciidoctor-latex"
 
 
 #     Application.get_env(:ruby_bridge, :asciidoc_path)
